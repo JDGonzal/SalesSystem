0b8da13867cc1622e000100add8fc05687a23e98
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-06-08 10:18:27 -0500

    02_Login/0211_AddingButtons
    - Se crean los componentes **`SaveButton.tsx`**, **`Line.tsx`** e **`Icon.tsx`**.
    - Se modifica el componente **`LoginTemplate.tsx`**.
    
    #02_Login/0211_AddingButtons

--- src/components/templates/LoginTemplate.tsx
+++ src/components/templates/LoginTemplate.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { Title, InputText2 } from '../../index.ts';
+import { Title, InputText2, SaveButton, Linea } from '../../index.ts';
+import {v} from '../../styles/variables.ts';
 
 const Container = styled.div`
   height: 100vh;
@@ -15,14 +16,43 @@ function LoginTemplate() {
         <div className='card'>
           <Title>Ingresar</Title>
           <form action=''>
-          <InputText2>
+            <InputText2>
               <label htmlFor='email'></label>
-              <input type='text' className='form__field' placeholder='email' name='email' id='email'/>
-            </InputText2>            <InputText2>
+              <input
+                type='text'
+                className='form__field'
+                placeholder='email'
+                name='email'
+                id='email'
+              />
+            </InputText2>{' '}
+            <InputText2>
               <label htmlFor='password'></label>
-              <input type='password' className='form__field' placeholder='password' name='password' id='password'/>
+              <input
+                type='password'
+                className='form__field'
+                placeholder='password'
+                name='password'
+                id='password'
+              />
+              <SaveButton
+                titulo='INGRESAR'
+                bgcolor='#1cb0f6'
+                color='255,255,255'
+                width='100%'
+              />
             </InputText2>
           </form>
+          <Linea>
+            <span>O</span>
+          </Linea>
+          <SaveButton
+            titulo='Google'
+            bgcolor='#fff'
+            color='0,0,0'
+            width='100%'
+            icono={<v.iconogoogle/>}
+          />
         </div>
       </section>
     </Container>
