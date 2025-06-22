24c554b05a83f1ae2e4ecc47a9f201316748077a
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-06-22 14:40:56 -0500

    03_Products/0308_StorageSetting
    - Instalar biblioteca `sweetalert2``.
    - Se añade código a **`crudCategories.tsx`**, en la función `InsertCategory()`.
    
    #03_Products/0308_StorageSetting

--- src/supabase/crudCategories.tsx
+++ src/supabase/crudCategories.tsx
@@ -1,23 +1,25 @@
-// import { supabase } from "./supabase.config";
 import { supabase } from '../index.ts';
+import Swal from 'sweetalert2';
 
-export async function InsertCategory(category: {
-  name: string;
-  color: string;
-  icon: string;
-  description: string;
-  id_company: number;
-  file: string;
-}) {
+export async function InsertCategory(
+  category: {
+    name: string;
+    color: string;
+    icon: string;
+    description: string;
+    id_company: number;
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  file: string
+) {
   const { data, error } = await supabase.rpc('fnc_category_insert', category);
-  // const { data, error } = await supabase
-  //   .from("categories")
-  //   .insert(category)
-  //   .select();
-
-  // if (error) {
-  //   throw new Error(`Error inserting category: ${error.message}`);
-  // }
-  // return data;
-  console.log('InsertCategory', data, error);
+  if (error) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: error.message,
+    });
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const new_category_id = data;//data?.[0]?.new_category_id;
 }
