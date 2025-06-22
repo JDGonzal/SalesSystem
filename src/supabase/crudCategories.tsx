b180e2a518ddc3eb30ee89d0c50d8a2abe61f1f8
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2025-06-22 18:09:15 -0500

    03_Products/0309_UploadingImage2Storage
    - Se añade la función `uploadImage()` en **`crudCategories.tsx`**, y se ajusta `InsertCategory()`.
    
    #03_Products/0309_UploadingImage2Storage

--- src/supabase/crudCategories.tsx
+++ src/supabase/crudCategories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { supabase } from '../index.ts';
 import Swal from 'sweetalert2';
 
@@ -9,8 +10,7 @@ export async function InsertCategory(
     description: string;
     id_company: number;
   },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  file: string
+  imageFile: File
 ) {
   const { data, error } = await supabase.rpc('fnc_category_insert', category);
   if (error) {
@@ -19,7 +19,37 @@ export async function InsertCategory(
       title: 'Oops...',
       text: error.message,
     });
+    return null;
   }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const new_category_id = data;//data?.[0]?.new_category_id;
+  const fileSize = imageFile.size;
+  if (fileSize != undefined) {
+    const new_category_id = data; //data?.[0]?.new_category_id;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const imageUrl = await uploadImage(new_category_id as string, imageFile);
+  }
+}
+
+async function uploadImage(category_id: string, imageFile: File) {
+  // const avatarFile = event.target.files[0]
+  const pathFile = 'categories/' + category_id;
+  const { data, error } = await supabase.storage
+    .from('images')
+    .upload(pathFile, imageFile, {
+      cacheControl: '2', // '3600' -> 1 hora,
+      upsert: true, // El archivo se reemplaza si ya existe
+    });
+  if (error) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: error.message,
+    });
+    return null;
+  }
+  if (data) {
+    const { data: urlData } = await supabase.storage
+      .from('images')
+      .getPublicUrl(pathFile);
+    return urlData.publicUrl;
+  }
 }
